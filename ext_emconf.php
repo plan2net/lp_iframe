dfb5a4d506d0444a8fa786b2250bb97bbab6146a
Wolfgang Klinger <wk@plan2.net>
2019-02-04 11:56:21 +0100

    Raised version compatibility to TYPO3 9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,7 +4,7 @@ $EM_CONF[$_EXTKEY] = array(
     'title' => 'iFrame for HTML5 Websites',
     'description' => 'Yet another iFrame extension with HTML5 support. Rendered with TypoScript. Doc: http://docs.typo3.org/typo3cms/extensions/lp_iframe/ and Git: https://github.com/lars85/TYPO3-Extensions-lp_iframe/',
     'category' => 'plugin',
-    'version' => '1.3.0',
+    'version' => '1.3.1',
     'state' => 'stable',
     'uploadfolder' => 0,
     'createDirs' => '',
@@ -14,11 +14,10 @@ $EM_CONF[$_EXTKEY] = array(
     'author_company' => '',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '6.0.0-8.7.99',
+            'typo3' => '6.0.0-9.5.99',
         ),
         'conflicts' => array(),
         'suggests' => array(),
     ),
-    'comment' => 'Updates for TYPO3 Version 7.6.x',
     'user' => 'lars85',
 );
